94349bd08e3f53f7e89dd896f241b244e9a37934
prasanthes23 <prasanthzorfa2001@gmail.com>
2026-04-22 15:03:22 +0530

    featurech2

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-fifth line 
+function getMessage() {
+    return "Hello from feature branch";
+}
+
+console.log(getMessage());
+console.log("Extra line 1");
+console.log("Extra line 2");
+console.log("Extra line 3");
